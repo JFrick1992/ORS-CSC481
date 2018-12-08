07e8a9e2bc6f260239dc789e2f688f40427f7f36
Jacob Frick <jacobfrick@jacobs-macbook-air-3.local>
2018-12-07 21:38:00 -0800

    Initial Commit

---
+++ ORS-CSC481/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ORS-CSC481
+//
+//  Created by Jacob Frick on 12/7/18.
+//  Copyright © 2018 Jacob Frick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
